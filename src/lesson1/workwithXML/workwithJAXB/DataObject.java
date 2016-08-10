dfe42ffda13054baeeedd840548ed09bd20f0918
Diukarev Sergii <vmesteprotiv@gmail.com>
2016-08-10 12:32:31 +0300

    add repository

---
+++ src/lesson1/workwithXML/workwithJAXB/DataObject.java
@@ -0,0 +1,23 @@
+package lesson1.workwithXML.workwithJAXB;
+
+import javax.xml.bind.annotation.*;
+import java.util.List;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlType(name = "dataObj")
+public class DataObject {
+
+    public int getI() {
+        return i;
+    }
+
+    @XmlElement
+    private int i = 19;
+
+    @XmlElement
+    @XmlElementWrapper(name = "elems")
+    List<CustomObject> custom;
+
+
+}
