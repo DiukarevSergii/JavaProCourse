b3e46ef9f41e5c2f0b5b9b0a9bcf4f76722f7cff
Diukarev Sergii <vmesteprotiv@gmail.com>
2016-08-15 17:23:25 +0300

    add examples

---
+++ src/lesson02/examples/JSON2/Clazz.java
@@ -0,0 +1,16 @@
+package lesson02.examples.JSON2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Clazz {
+    int i = 90;
+
+
+    List<Object> names = new ArrayList<>(Arrays.asList("Jack", "Tom", "Mickle", 10, 'w'));
+
+    public int getI() {
+        return i;
+    }
+}
